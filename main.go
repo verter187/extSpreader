c6736da84e9334de7ffe3a246ec4a17b8fbe8016
verter187 <verter187@inbox.ru>
2023-05-09 18:40:03 +0300

    Структурировал код и вынес в функции

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+func moveFiles(newP string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		// Walk will not walk into path directory
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			fname := filepath.Base(path)
+			cExt := filepath.Ext(fname)
+			cExt = strings.TrimLeft(cExt, ".")
+
+			ctlgExt := filepath.Join(newP, cExt)
+
+			err = os.MkdirAll(ctlgExt, 0766)
+
+			if err == nil {
+				err = os.Rename(filepath.Join(path), filepath.Join(ctlgExt, fname))
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	}
+}
+
 func main() {
 
 	oldP := "/home/wurtow977/Development/others/testfiles1/"
 	newP := "/home/wurtow977/Development/others/newfiles1/"
-	err := filepath.Walk(oldP,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
 
-			if !info.IsDir() {
-				fname := filepath.Base(path)
-				cExt := filepath.Ext(fname)
-				cExt = strings.Replace(cExt, ".", "", -1)
-
-				ctlgExt := filepath.Join(newP, cExt)
-
-				err = os.MkdirAll(ctlgExt, 0766)
-				fmt.Println(ctlgExt)
-				if err == nil {
-					err = os.Rename(filepath.Join(path), filepath.Join(ctlgExt, fname))
-					if err != nil {
-						fmt.Println(err)
-						return err
-					}
-				}
-
-				fmt.Println(fname)
-				return nil
-			}
-			return nil
-		})
+	err := filepath.Walk(oldP, moveFiles(newP))
 	if err != nil {
-		log.Println(err)
+		log.Fatal("888", err)
 	}
 
 }
