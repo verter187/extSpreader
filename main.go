c79c342692cb176f6d78b8f7f2e3b9c5e13cbf8a
verter187 <verter187@inbox.ru>
2023-05-09 20:13:20 +0300

    Добавил создание архива первоначального каталога

--- main.go
+++ main.go
@@ -1,11 +1,86 @@
 package main
 
 import (
+	"archive/zip"
+	"io"
+
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+func zipit(source, target string, needBaseDir bool) error {
+	zipfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipfile.Close()
+
+	archive := zip.NewWriter(zipfile)
+	defer archive.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			if needBaseDir {
+				header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+			} else {
+				path := strings.TrimPrefix(path, source)
+				if len(path) > 0 && (path[0] == '/' || path[0] == '\\') {
+					path = path[1:]
+				}
+				if len(path) == 0 {
+					return nil
+				}
+				header.Name = path
+			}
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
+}
 
 func moveFiles(newP string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
@@ -23,7 +98,7 @@ func moveFiles(newP string) filepath.WalkFunc {
 			err = os.MkdirAll(ctlgExt, 0766)
 
 			if err == nil {
-				err = os.Rename(filepath.Join(path), filepath.Join(ctlgExt, fname))
+				err = os.Rename(path, filepath.Join(ctlgExt, fname))
 				if err != nil {
 					return err
 				}
@@ -36,12 +111,32 @@ func moveFiles(newP string) filepath.WalkFunc {
 
 func main() {
 
-	oldP := "/home/wurtow977/Development/others/testfiles1/"
-	newP := "/home/wurtow977/Development/others/newfiles1/"
-
-	err := filepath.Walk(oldP, moveFiles(newP))
+	curP := "/home/wurtow977/Development/others/testfiles1/"
+	ncurP := filepath.Base(curP)
+	tmpP, err := os.MkdirTemp("", "tmpP")
 	if err != nil {
-		log.Fatal("888", err)
+		log.Fatal(err)
+		return
+	}
+	defer os.RemoveAll(tmpP) // clean up
+
+	err = zipit(curP, filepath.Join(tmpP, "old_"+ncurP+".zip"), true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = filepath.Walk(curP, moveFiles(tmpP))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.RemoveAll(curP)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Rename(tmpP, curP)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 }
